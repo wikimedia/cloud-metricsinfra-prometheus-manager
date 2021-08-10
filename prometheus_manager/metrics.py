133de9c9ca755b0562a524c55f264d41b3386dcb
Taavi Väänänen <hi@taavi.wtf>
2021-08-10 19:23:40 +0300

    metrics: Group by route, not by url
    Change-Id: I14c4350b747d91b19260adf2fbeaa86a57141f81

--- prometheus_manager/metrics.py
+++ prometheus_manager/metrics.py
@@ -4,6 +4,6 @@ from prometheus_flask_exporter import PrometheusMetrics
 from prometheus_flask_exporter.multiprocess import UWsgiPrometheusMetrics
 
 if 'PROMETHEUS_MULTIPROC_DIR' in os.environ:
-    metrics = UWsgiPrometheusMetrics.for_app_factory()
+    metrics = UWsgiPrometheusMetrics.for_app_factory(group_by='url_rule')
 else:
-    metrics = PrometheusMetrics.for_app_factory(path=None)
+    metrics = PrometheusMetrics.for_app_factory(group_by='url_rule', path=None)
