fdadb7ab057ad94e5781fb195fc1fde8514faa1c
Taavi Väänänen <hi@taavi.wtf>
2022-11-15 10:46:22 +0200

    Properly configure Python packages when installing
    Change-Id: I9660c00c23af213e776e3608874af74d567acd8b

--- setup.py
+++ setup.py
@@ -5,9 +5,9 @@ from setuptools import find_packages, setup
 current_directory = path.dirname(path.abspath(__file__))
 
 setup(
-    name='prometheus-manager',
+    name='prometheus_manager',
     version='0.0.1',
-    packages=find_packages('prometheus_manager'),
+    packages=find_packages(),
     install_requires=[
         line.strip() for line in open(path.join(current_directory, 'requirements.txt'), 'r')
     ],
